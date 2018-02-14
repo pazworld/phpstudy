3a7c8cfc7a7b5a09f6bd413242152a3ad8538986
Pazworld <paz@moudamepo.net>
2018-02-14 19:32:41 +0900

    Add register function.

--- bookapp/app/Http/routes.php
+++ bookapp/app/Http/routes.php
@@ -28,7 +28,18 @@ Route::group(['middleware' => ['web']], function () {
 	]);
     });
     Route::post('/book', function(Request $request){
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255'
+	]);
+        if($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $book = new Book; // Eloquent ORM
+        $book->title = $request->name;
+        $book->save();
+        return redirect('/');
     });
     Route::delete('/book/{book}', function(Book $book){
         //
